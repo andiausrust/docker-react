8f7b5f4c593b0c4cb2719ac946b3d8cf7a291973
andiausrust <familie.mayer@aon.at>
2018-11-02 08:50:34 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> Hi there from Andi again
+            Edit <code>src/App.js</code> Hi there from Andi again now online
           </p>
           <a
             className="App-link"
